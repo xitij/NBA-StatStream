342430af35432498fd2ac8f3cf4e2b0e79364b47
Tarik Ourchane <tlourchane@gmail.com>
2014-01-14 15:19:49 -0800

    -if customView is null (hasn't been created yet) a placeholder view is now inflated and returned. This stops the app from crashing on  recreate of the ArchivedGame activity after it is removed from memory. -Override of onSaveInstanceState. -Removed some unused imports.

--- NBAStatStream/src/com/jitix/nbastatstream/AdvancedBoxScoreFragment.java
+++ NBAStatStream/src/com/jitix/nbastatstream/AdvancedBoxScoreFragment.java
@@ -1,32 +1,22 @@
 package com.jitix.nbastatstream;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import com.jitix.nbastatstream.NBAStatStream.TeamInfo;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
 public class AdvancedBoxScoreFragment extends Fragment {
-	
+
 	private static final String TAG = "NBAStatStream";
 	private static final String ADV_BOX = "advvanced_box";
 	public static final String TEAM_NUM = "team_number";
 	static final int AWAY_TEAM = 0;
 	static final int HOME_TEAM = 1;
-	
+
 	private BasketballGame savedGame;
-	
+
 	//
 	// newInstance: Returns an instance of the ArchivedGameFragment. Takes i (page_num)
 	// 	as argument. It creates a new instance if it doesn't exist otherwise returns
@@ -34,18 +24,18 @@ public class AdvancedBoxScoreFragment extends Fragment {
 	//
 	public static AdvancedBoxScoreFragment newInstance(int i, boolean advancedBox) {
 		AdvancedBoxScoreFragment fragment = new AdvancedBoxScoreFragment();
-		
+
 		Log.d(TAG, "newInstance called for AdvancedBoxScoreFragment with i = " + i);
-		
+
 		// Put i in the arguments
 		Bundle args = new Bundle();
 		args.putInt(TEAM_NUM, i);
 		args.putBoolean(ADV_BOX, advancedBox);
 		fragment.setArguments(args);
-		
+
 		return fragment;
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,15 +45,15 @@ public class AdvancedBoxScoreFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
+
 		Bundle args = getArguments();
 		int team_num = args.getInt(TEAM_NUM, -1);
 		boolean adv_box = args.getBoolean(ADV_BOX);
-		
+
 		ArchivedGameFragment parentFrag = (ArchivedGameFragment) getParentFragment();
 		int page_num = parentFrag.getArguments().getInt(ArchivedGameFragment.PAGE_NUM);
-		Log.d(TAG, "Parent Fragment page_num = " + page_num + ", team_num = " + team_num + ", advanced box = " + adv_box);
-		
+		Log.d(TAG, "AdancedBoxScoreFragment : onCreateView : Parent Fragment page_num = " + page_num + ", team_num = " + team_num + ", advanced box = " + adv_box);
+
 		// Get the View for either the Advanced Box or Box
 		ViewGroup customView;
 		if(adv_box) {
@@ -71,28 +61,32 @@ public class AdvancedBoxScoreFragment extends Fragment {
 		} else {
 			customView = parentFrag.getBoxView(team_num);
 		}
-		
+
 		if(customView == null) {
-			Log.d(TAG, "AdvancedBoxScoreFragment : onCreateView : customView == null");
-		} else {
-			Log.d(TAG, "AdvancedBoxScoreFragment : onCreateView : viewId = " + customView.getId());
-			Log.d(TAG, "adv_box_view_away = " + R.id.adv_box_view_away + ", adv_box_view_home = " + R.id.adv_box_view_home);
-			Log.d(TAG, "box_view_away = " + R.id.box_view_away + ", box_view_home = " + R.id.box_view_home);
+			Log.d(TAG, "AdvancedBoxScoreFragment : onCreateView : customView == null. Providing a filler.");
+			customView = (ViewGroup) inflater.inflate(R.layout.box_update, container, false);
 		}
-		
+
 		if(customView.getParent() != null) {
-			Log.d(TAG, "AdvancedBoxScoreFragment : onCreateView : customView has parent already, removing it");
+			Log.d(TAG, "AdvancedBoxScoreFragment : onCreateView : customView has parent already, removing it.");
 			ViewGroup parent = (ViewGroup) customView.getParent();
 			parent.removeView(customView);
 		}
-		
+
 		return customView;
 	}
-	
+
 	@Override
 	public void onStop() {
 		super.onStop();
 		// Save the update flag in the bundle
 		Bundle args = getArguments();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		Log.d(TAG, "AdvancedBoxScoreFragment : onSaveInstanceState");
 	}
 }
